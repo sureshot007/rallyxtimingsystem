7372b10370715ff404f31afe4749018f5c0fe6b9
sureshot007 <sureshot007@gmail.com>
2016-05-17 16:30:00 -0400

    Update

--- app/src/main/java/com/example/jperrin/rallyxtimingsystem/Main.java
+++ app/src/main/java/com/example/jperrin/rallyxtimingsystem/Main.java
@@ -2,12 +2,45 @@ package com.example.jperrin.rallyxtimingsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Main extends AppCompatActivity {
+
+
+    LinearLayout menuWindow;
+    LinearLayout eventSelectionScreen;
+    Button selectEventButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Layout Screens
+        menuWindow = (LinearLayout) findViewById(R.id.MenuWindow);
+        eventSelectionScreen = (LinearLayout) findViewById(R.id.EventSelectionScreen);
+
+        //Buttons
+        selectEventButton = (Button) findViewById(R.id.selectEventButton);
+
+
     }
-}
+
+
+    public void selectEventButtonClicked(View v) {
+        menuWindow.setVisibility(View.GONE);
+        eventSelectionScreen.setVisibility(View.VISIBLE);
+    }
+
+
+
+
+
+
+
+
+}//EOL
+
