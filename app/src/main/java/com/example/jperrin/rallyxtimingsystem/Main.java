30f2590eae93a61e5981aab2f01e0d542b158572
sureshot007 <sureshot007@gmail.com>
2016-05-17 21:27:56 -0400

    Coded the screens and some buttons

--- app/src/main/java/com/example/jperrin/rallyxtimingsystem/Main.java
+++ app/src/main/java/com/example/jperrin/rallyxtimingsystem/Main.java
@@ -9,9 +9,23 @@ import android.widget.LinearLayout;
 public class Main extends AppCompatActivity {
 
 
-    LinearLayout menuWindow;
-    LinearLayout eventSelectionScreen;
-    Button selectEventButton;
+    LinearLayout menuWindow,
+            eventSelectionScreen,
+            clockSyncScreen,
+            registrationScreen,
+            startScreen,
+            finishScreen,
+            resultsScreen;
+    Button selectEventButton,
+            eventBackButton,
+            syncButton,
+            clockSyncButton,
+            registrationButton,
+            startButton,
+            finishButton,
+            resultsButton,
+            addDriverButton,
+            doneWithRegistrationButton;
 
 
     @Override
@@ -22,24 +36,77 @@ public class Main extends AppCompatActivity {
         // Layout Screens
         menuWindow = (LinearLayout) findViewById(R.id.MenuWindow);
         eventSelectionScreen = (LinearLayout) findViewById(R.id.EventSelectionScreen);
+        clockSyncScreen = (LinearLayout) findViewById(R.id.ClockSyncScreen);
+        registrationScreen = (LinearLayout) findViewById(R.id.RegistrationScreen);
+        startScreen = (LinearLayout) findViewById(R.id.StartScreen);
+        finishScreen = (LinearLayout) findViewById(R.id.FinishScreen);
+        resultsScreen = (LinearLayout) findViewById(R.id.ResultsScreen);
 
         //Buttons
         selectEventButton = (Button) findViewById(R.id.selectEventButton);
-
+        eventBackButton = (Button) findViewById(R.id.eventBackButton);
+        syncButton = (Button) findViewById(R.id.syncButton);
+        clockSyncButton = (Button) findViewById(R.id.clockSyncButton);
+        registrationButton = (Button) findViewById(R.id.registrationButton);
+        startButton = (Button) findViewById(R.id.startButton);
+        finishButton = (Button) findViewById(R.id.finishButton);
+        resultsButton = (Button) findViewById(R.id.resultsButton);
+        addDriverButton = (Button) findViewById(R.id.addDriverButton);
+        doneWithRegistrationButton = (Button) findViewById(R.id.doneWithRegistrationButton);
 
     }
 
+    public void backToMenu(View v){
+        resultsScreen.setVisibility(View.GONE);
+        finishScreen.setVisibility(View.GONE);
+        startScreen.setVisibility(View.GONE);
+        registrationScreen.setVisibility(View.GONE);
+        clockSyncButton.setVisibility(View.GONE);
+        eventSelectionScreen.setVisibility(View.GONE);
+        menuWindow.setVisibility(View.VISIBLE);
+    }
+
+    public void registrationButtonClicked(View v){
+        menuWindow.setVisibility(View.GONE);
+        registrationScreen.setVisibility(View.VISIBLE);
+    }
+    public void startButtonClicked(View v){
+        menuWindow.setVisibility(View.GONE);
+        startScreen.setVisibility(View.VISIBLE);
+    }
+    public void finishButtonClicked(View v){
+        menuWindow.setVisibility(View.GONE);
+        finishScreen.setVisibility(View.VISIBLE);
+    }
+    public void resultsButtonClicked(View v){
+        menuWindow.setVisibility(View.GONE);
+        resultsScreen.setVisibility(View.VISIBLE);
+    }
+
+    public void doneWithRegistrationButtonClicked(View v){
+        registrationScreen.setVisibility(View.GONE);
+        menuWindow.setVisibility(View.VISIBLE);
+    }
 
     public void selectEventButtonClicked(View v) {
         menuWindow.setVisibility(View.GONE);
         eventSelectionScreen.setVisibility(View.VISIBLE);
     }
 
+    public void eventBackButtonClicked(View v){
+        eventSelectionScreen.setVisibility(View.GONE);
+        menuWindow.setVisibility(View.VISIBLE);
+    }
 
+    public void syncButtonClicked(View v){
+        clockSyncScreen.setVisibility(View.GONE);
+        menuWindow.setVisibility(View.VISIBLE);
+    }
 
-
-
-
+    public void clockSyncButtonClicked(View v){
+        menuWindow.setVisibility(View.GONE);
+        clockSyncScreen.setVisibility(View.VISIBLE);
+    }
 
 
 }//EOL
